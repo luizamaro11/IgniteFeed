a420d799404d6d4cac2d993ba941e4a3709e1535
luizamaro11 <luiz.amaro@gmail.com>
2024-11-18 17:00:15 -0300

    Projeto Ignite Feed

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+// JAX = JavaScript + XML
+import { Header } from './components/Header';
+import { Post, PostType } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
+import './global.css';
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luizamaro11.png',
+      name: 'Luiz Henrique',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph' as "paragraph", content: 'Fala galeraa 👋'},
+      { type: 'paragraph' as "paragraph", content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link' as "link", content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-11-10 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rockeatseat'
+    },
+    content: [
+      { type: 'paragraph' as "paragraph", content: 'Fala galeraa 👋'},
+      { type: 'paragraph' as "paragraph", content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link' as "link", content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-11-12 20:00:00')
+  }
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from 'phosphor-react';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} 
+        src="https://github.com/luizamaro11.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luiz Henrique</strong>
+              <time title="11 de maio às 19:00h" dateTime="2024-11-11 19:00:00">Cerda de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
